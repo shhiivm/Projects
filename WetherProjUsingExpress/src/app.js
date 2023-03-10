ee9137aaa09cb9d263f26f079799d7c6715267a9
shhiivm <shhiivm@pm.me>
2023-03-10 18:51:00 +0530

    modified

--- WetherProjUsingExpress/src/app.js
+++ WetherProjUsingExpress/src/app.js
@@ -1,28 +1,36 @@
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const request = require('request')
 const app = express();
 
-const port = 5001  || process.env.PORT;
+const port = process.env.PORT || 3000;
 
-const staticPath = path.join(__dirname,"../public")
+const staticPath = path.join(__dirname,"../public");
+const templates_path = path.join(__dirname,"..templates/views");
+const partials_path = path.join(__dirname,"..templates/partials");
 
-app.use(express.static(staticPath))
+app.set('view engine','hbs');
+app.set('views', templates_path);
+hbs.registerPartials(partials_path);
+
+app.use(express.static(staticPath));
+
 
 
 //Routing
-app.get('/',(req,res)=>{
-    res.send("Welcome to express")
+app.get("",(req,res)=>{
+    res.render('index')
 })
 app.get('/about',(req,res)=>{
-    res.send("Welcome to express, About page")
+    res.render('about')
 })
 app.get('/weather',(req,res)=>{
-    res.send("Welcome to weather ")
+    res.render('weather')
 })
 
 app.get('*',(req,res)=>{
-    res.send("404 ERROR")
+    res.render('404error')
 })
 
 app.listen(port,()=>{
